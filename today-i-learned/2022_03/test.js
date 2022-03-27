e7145ea275a3f61ae2cce202e9c5bc33bb6c1c92
Haedong Kim <haedong1111@gmail.com>
2022-03-27 21:41:17 +0900

    Create til_220327

--- today-i-learned/2022_03/test.js
+++ today-i-learned/2022_03/test.js
@@ -1,25 +1,16 @@
-const [n, r] = [4, 3];
-const visited = new Array(n);
-const part = [];
-const result = [];
+// 원래의 참고 table
+const originalTable = [
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+];
 
-dfs(0, 0);
-
-console.log(result);
-
-function dfs(depth, idx) {
-  if (depth === r) {
-    result.push([...part]);
-    return;
-  }
-
-  for (let i = idx; i < n; i++) {
-    if (!visited[i]) {
-      visited[i] = true;
-      part.push(i + 1);
-      dfs(depth + 1, i);
-      part.pop();
-      visited[i] = false;
-    }
-  }
-}
+// zero padding을 추가한 참고 table
+const zeroPaddingTable = [
+  [0, 0, 0, 0, 0],
+  [0, 1, 2, 3, 4],
+  [0, 1, 2, 3, 4],
+  [0, 1, 2, 3, 4],
+  [0, 1, 2, 3, 4],
+];
