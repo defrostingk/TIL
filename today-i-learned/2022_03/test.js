4d4e49dd3cfc2ad69b9d5e0255fd6eecdffda667
Haedong Kim <haedong1111@gmail.com>
2022-03-29 16:26:35 +0900

    Create til_220329.md

--- today-i-learned/2022_03/test.js
+++ today-i-learned/2022_03/test.js
@@ -1,16 +1,21 @@
-// 원래의 참고 table
-const originalTable = [
-  [1, 2, 3, 4],
-  [1, 2, 3, 4],
-  [1, 2, 3, 4],
-  [1, 2, 3, 4],
-];
+function bfs(start) {
+  // 방문할 node를 담을 queue를 생성한다.
+  const queue = [];
+  queue.push(start);
 
-// zero padding을 추가한 참고 table
-const zeroPaddingTable = [
-  [0, 0, 0, 0, 0],
-  [0, 1, 2, 3, 4],
-  [0, 1, 2, 3, 4],
-  [0, 1, 2, 3, 4],
-  [0, 1, 2, 3, 4],
-];
+  // queue가 빌 때까지, 모든 node를 방문할 때까지 반복한다.
+  while (queue.length) {
+    nodeCurrent = queue.shift();
+    if (!visited[nodeCurrent]) {
+      visited[nodeCurrent] = true;
+      orderBfs.push(nodeCurrent);
+      for (let i = 0; i < adjList[nodeCurrent].length; i++) {
+        nodeNext = adjList[nodeCurrent][i];
+        if (!visited[nodeNext]) {
+          // 다음 방문할 node를 queue에 저장한다.
+          queue.push(nodeNext);
+        }
+      }
+    }
+  }
+}
