d694ca47b16f0d00fb6bb442cec9a78f3d1ce5a3
Haedong Kim <haedong1111@gmail.com>
2022-04-16 21:13:08 +0900

    TIL: Create 220416

--- today-i-learned/2022_04/test.js
+++ today-i-learned/2022_04/test.js
@@ -1,4 +1,22 @@
-function fun() {
-  console.log(this);
+const tree = {
+  A: ['B', 'C'],
+  B: ['D', '.'],
+  C: ['E', 'F'],
+  E: ['.', '.'],
+  F: ['.', 'G'],
+  D: ['.', '.'],
+  G: ['.', '.'],
+};
+
+let result = '';
+
+function preorder(node) {
+  if (node === '.') return;
+  const [left, right] = tree[node];
+  result += node;
+  preorder(left);
+  preorder(right);
 }
-fun();
+
+preorder('A');
+console.log(result);
---
+++ today-i-learned/2022_04/test.py
@@ -0,0 +1,5 @@
+
+file_input = open('/mnt/repository/til/today-i-learned/2022_04/input.txt')
+
+for line in file_input:
+    print(line)
