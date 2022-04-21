5177b749468da1a99c9591b8e746d5d3c8ad073b
Haedong Kim <haedong1111@gmail.com>
2022-04-22 00:41:43 +0900

    TIL: Create 220421 & JSON: npm

---
+++ books/clean-code/test.js
@@ -0,0 +1,5 @@
+const map = {};
+
+map["toString"] = 1;
+
+console.log(map);
--- today-i-learned/2022_04/test.js
+++ today-i-learned/2022_04/test.js
@@ -1,22 +1,3 @@
-const tree = {
-  A: ['B', 'C'],
-  B: ['D', '.'],
-  C: ['E', 'F'],
-  E: ['.', '.'],
-  F: ['.', 'G'],
-  D: ['.', '.'],
-  G: ['.', '.'],
-};
-
-let result = '';
-
-function preorder(node) {
-  if (node === '.') return;
-  const [left, right] = tree[node];
-  result += node;
-  preorder(left);
-  preorder(right);
-}
-
-preorder('A');
+const queryString = require('query-string');
+const result = queryString.parse('person[name]=tom&person[age]=3');
 console.log(result);
