0a90a1bd7a0248b765048d818ab4d996d0cc618d
Haedong Kim <haedong1111@gmail.com>
2022-05-24 12:11:11 +0900

    JS: Create 08_control-flow-statement

--- javascript/test.js
+++ javascript/test.js
@@ -1,3 +1,8 @@
-var a, b, c;
-a = b = c = 0;
-console.log(a, b, c);
+outer: for (let i = 0; i < 3; i++) {
+  for (let j = 0; j < 3; j++) {
+    if (i + j === 3) break outer;
+    console.log(`inner [${i}, ${j}]`);
+  }
+}
+
+console.log('done');
