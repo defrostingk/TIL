1c48344bca2d4cf887cce4f3875e82ea4aa22df8
Haedong Kim <haedong1111@gmail.com>
2022-05-30 21:18:17 +0900

    JS: Create 10_object-literal

--- javascript/test.js
+++ javascript/test.js
@@ -1,6 +1,10 @@
-var elem = null;
-// var value = elem.value; // TypeError: Cannot read property 'value' of null
+const prefix = 'prop';
+let i = 0;
 
-var value = elem && elem.value; // null
+const obj = {
+  [`${prefix}-${++i}`]: i,
+  [`${prefix}-${++i}`]: i,
+  [`${prefix}-${++i}`]: i,
+};
 
-console.log(value);
+console.log(obj); // { 'prop-1': 1, 'prop-2': 2, 'prop-3': 3 }
