949deeabb4ae1b89d7b3cc554e476ada23619742
Haedong Kim <haedong1111@gmail.com>
2022-06-03 19:08:55 +0900

    JS: Add contents to 12_function

--- javascript/test.js
+++ javascript/test.js
@@ -1,10 +1,11 @@
-const prefix = 'prop';
-let i = 0;
+function repeat(n, f) {
+  for (let i = 0; i < n; i++) {
+    f(i);
+  }
+}
 
-const obj = {
-  [`${prefix}-${++i}`]: i,
-  [`${prefix}-${++i}`]: i,
-  [`${prefix}-${++i}`]: i,
+const logAll = function (i) {
+  console.log(i);
 };
 
-console.log(obj); // { 'prop-1': 1, 'prop-2': 2, 'prop-3': 3 }
+repeat(5, logAll); // 0 1 2 3 4
