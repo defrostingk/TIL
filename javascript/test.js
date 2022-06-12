58578162acabeb11f55992025769fd4f4a357c7c
Haedong Kim <haedong1111@gmail.com>
2022-06-12 19:21:42 +0900

    JS: Create 14_problems_of_global_variables

--- javascript/test.js
+++ javascript/test.js
@@ -1,11 +1,9 @@
-function repeat(n, f) {
-  for (let i = 0; i < n; i++) {
-    f(i);
-  }
+var x = 'global';
+
+function foo() {
+  console.log(x);
+  var x = 'local';
 }
 
-const logAll = function (i) {
-  console.log(i);
-};
-
-repeat(5, logAll); // 0 1 2 3 4
+foo();
+console.log(x);
